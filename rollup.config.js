75efaa77c73e5dfb0a1a6da3307577de94be560b
skratchdot <git@skratchdot.com>
2018-02-04 13:29:17 -0500

    start using rollup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: 'lib/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs',
+    exports: 'named'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers']
+    }),
+    postcss({
+      plugins: []
+    })
+  ],
+  external: [ 'moment', 'jquery', 'react', 'react-dom', 'prop-types' ]
+};

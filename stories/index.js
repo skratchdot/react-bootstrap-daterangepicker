6ce8ce4471c9b62ea9a6d7874d6f94a353766109
skratchdot <git@skratchdot.com>
2018-02-09 00:57:27 -0500

    adding story w/ moment() for startDate

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
@@ -25,6 +26,16 @@ storiesOf('DateRangePicker', module)
     const endDate = text('endDate', '1/5/2017');
     return (
       <DateRangePicker startDate={startDate} endDate={endDate}>
+        <button>{buttonLabel}</button>
+      </DateRangePicker>
+    );
+  })
+  .add('with start date as moment() and end date as string', () => {
+    const buttonLabel = text('label', 'click to open');
+    const startDate = text('startDate', '1/1/2017');
+    const endDate = text('endDate', '1/5/2017');
+    return (
+      <DateRangePicker startDate={moment(startDate)} endDate={endDate}>
         <button>{buttonLabel}</button>
       </DateRangePicker>
     );
